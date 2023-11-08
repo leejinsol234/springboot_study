e03090219ea64ac963711d0b9fe9e8c40b911138
leejinsol234 <desirable7@naver.com>
2023-11-08 17:55:01 +0900

    Spring Data jdbc, JSON 응답과 요청 처리

--- exam01/src/main/java/org/koreait/schedules/PostStat.java
+++ exam01/src/main/java/org/koreait/schedules/PostStat.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class PostStat {
     // @Scheduled(cron="0 0 1 * * *") 새벽 1시마다
     //@Scheduled(cron = "*/5 * * * * *") //5초마다
-    //@Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS) //5초 마다 (고정간격으로)실행. 실행 완료와 상관없이 정해진 간격마다 실행됨.
+    //@Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS) //5초 마다 (고정 간격으로)실행. 실행 완료와 상관없이 정해진 간격마다 실행됨.
     //@Scheduled(initialDelay = 5, timeUnit = TimeUnit.SECONDS) //작업 시작 전 5초 대기
     public void process(){
         //System.out.println("게시글 통계 수행");
